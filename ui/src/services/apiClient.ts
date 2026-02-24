fdf4094b77da67840e0dcfd61a371d6dabf1863c
Cevdet Tufan <cevdet.tufan@hotmail.com>
2026-02-24 21:42:21 +0300

    fix: localhost api url

--- ui/src/services/apiClient.ts
+++ ui/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://localhost:7142',
+    baseURL: '',
     headers: {
         'Content-Type': 'application/json',
     },
